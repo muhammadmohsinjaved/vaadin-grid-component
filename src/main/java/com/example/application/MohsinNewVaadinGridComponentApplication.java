ea0b71fff8529c8a62e6c39ae54b4171a7678700
Mohsin Javed <mohsin9042002@gmail.com>
2024-11-19 10:46:47 +0500

    V16

---
+++ src/main/java/com/example/application/MohsinNewVaadinGridComponentApplication.java
@@ -0,0 +1,12 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohsinNewVaadinGridComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohsinNewVaadinGridComponentApplication.class, args);
+    }
+}
